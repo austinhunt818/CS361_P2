eee120f0b87e63d93e506cb018593771423bc2de
Austin Hunt <austinhunt818@u.boisestate.edu>
2024-10-22 10:58:16 -0600

    Skeletons for NFA and NFAState classes

---
+++ fa/nfa/NFAState.java
@@ -0,0 +1,23 @@
+package fa.nfa;
+
+import fa.State;
+
+public class NFAState extends State{
+    public NFAState() {
+        super();
+    }
+
+    public NFAState(String name) {
+        super(name);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
