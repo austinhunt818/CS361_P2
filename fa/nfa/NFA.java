22c084d2b0da0d9717e25a8110ce0dcb8640b2c4
Austin Hunt <austinhunt818@u.boisestate.edu>
2024-10-22 11:42:43 -0600

    Implemented NFAState and addState, setStart, setFinal, getState, and addSigma for NFA

--- fa/nfa/NFA.java
+++ fa/nfa/NFA.java
@@ -2,28 +2,49 @@ package fa.nfa;
 
 import fa.State;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class NFA implements NFAInterface {
 
+    Set<NFAState> states = new LinkedHashSet<>();
+    Set<Character> symbols = new LinkedHashSet<>();
+
+    NFAState startState;
+    NFAState finalState;
+
     @Override
     public boolean addState(String name) {
-        return false;
+        if(states.contains(getStateByName(name, states))){
+            return false;
+        }
+        states.add(new NFAState(name));
+        return true;
     }
 
     @Override
     public boolean setFinal(String name) {
-        return false;
+        NFAState state = getStateByName(name, states);
+        if (state == null) {
+            return false; // State does not exist
+        }
+        finalState = state;
+        return true;
     }
 
     @Override
     public boolean setStart(String name) {
-        return false;
+        NFAState state = getStateByName(name, states);
+        if (state == null) {
+            return false; // State does not exist
+        }
+        startState = state;
+        return true;
     }
 
     @Override
     public void addSigma(char symbol) {
-
+        symbols.add(symbol);
     }
 
     @Override
@@ -38,7 +59,7 @@ public class NFA implements NFAInterface {
 
     @Override
     public NFAState getState(String name) {
-        return new NFAState();
+        return getStateByName(name, states);
     }
 
     @Override
@@ -68,11 +89,27 @@ public class NFA implements NFAInterface {
 
     @Override
     public boolean addTransition(String fromState, Set<String> toStates, char onSymb) {
-        return false;
+
     }
 
     @Override
     public boolean isDFA() {
         return false;
+    }
+
+    /**
+     * A private getter for a specific state from the specified set to help with getting DFAStates
+     * @author Austin Hunt
+     * @param target the name of the target state
+     * @param stateSet the set of states to get target from
+     * @return the state associated with the name, or null of it isn't in the set
+     */
+    private NFAState getStateByName(String target, Set<NFAState> stateSet) {
+        for (NFAState curr : stateSet) {
+            if (curr.getName().equals(target)) {
+                return curr;
+            }
+        }
+        return null;
     }
 }
--- fa/nfa/NFAState.java
+++ fa/nfa/NFAState.java
@@ -2,7 +2,12 @@ package fa.nfa;
 
 import fa.State;
 
+import java.util.*;
+
 public class NFAState extends State{
+
+    Map<Character, Set<NFAState>> transitions = new HashMap<>();
+
     public NFAState() {
         super();
     }
@@ -14,6 +19,22 @@ public class NFAState extends State{
     @Override
     public String getName() {
         return super.getName();
+    }
+
+    /**
+     * Adds a transition to a specified state on the specified symbol
+     * @param toState the state to transition to
+     * @param symbol the symbol to transition on
+     */
+    public void addTransition(NFAState toState, char symbol){
+        if(transitions.containsKey(symbol)){
+            transitions.get(symbol).add(toState);
+        }
+        else {
+            Set<NFAState> transitionStates = new LinkedHashSet<>();
+            transitionStates.add(toState);
+            transitions.put(symbol, transitionStates);
+        }
     }
 
     @Override
